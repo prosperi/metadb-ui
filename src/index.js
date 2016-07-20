06a3803ad327c309e4f9aa12992d43d817760f78
Adam Malantonio <malantoa@lafayette.edu>
2016-07-20 09:15:18 -0400

    move routing to src/index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
-'use strict'
-
 // yr main app
 import React from 'react'
 import { Provider } from 'react-redux'
 import { render } from 'react-dom'
-import routes from './routes'
-import store from './store'
+import Router from 'react-router/lib/Router'
+import Route from 'react-router/lib/Route'
+import IndexRoute from 'react-router/lib/IndexRoute'
 
-render(
+import store, { history } from './store'
+
+// pages / wrappers
+import App from './App.jsx'
+import Home from './pages/Home.jsx'
+import WorkEdit from './pages/WorkEdit.jsx'
+import About from './pages/About.jsx'
+
+import CollectionWrapper from './pages/CollectionWrapper.jsx'
+import CollectionHome from './pages/CollectionHome.jsx'
+
+const MetaDB = (
 <Provider store={store}>
-	{routes}
+	<Router history={history}>
+		<Route path="/" component={App}>
+			<IndexRoute component={Home} />
+			<Route path="/about" component={About} />
+
+			<Route path="/:collectionId" component={CollectionWrapper}>
+				<IndexRoute component={CollectionHome} />
+			</Route>
+
+		</Route>
+	</Router>
 </Provider>
-, 
-document.querySelector('#app'))
+)
+
+render(MetaDB, document.querySelector('#app'))
