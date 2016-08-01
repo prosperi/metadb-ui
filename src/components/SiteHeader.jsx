31f1a36edfbf8310e7c5c4adc935f02ae54d977f
Adam Malantonio <malantoa@lafayette.edu>
2016-08-01 14:52:03 -0400

    add placeholder nav links to SiteHeader

--- src/components/SiteHeader.jsx
+++ src/components/SiteHeader.jsx
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react'
 import IndexLink from 'react-router/lib/IndexLink'
+import Link from 'react-router/lib/Link'
 
 const SiteHeader = React.createClass({
 	render: function () {
@@ -7,6 +8,34 @@ const SiteHeader = React.createClass({
 		<header className="site-header navbar">
 			<div className="site-title">
 				<IndexLink to="/">MetaDB</IndexLink>
+			</div>
+
+			<div className="user-options">
+				<ul className="site-navigation">
+					<li>
+						<Link to={`/search`}>
+							Search
+						</Link>
+					</li>
+
+					<li>
+						<Link to={`/collections`}>
+							Collections
+						</Link>
+					</li>
+
+					<li>
+						<Link to={`/vocabularies`}>
+							Vocabularies
+						</Link>
+					</li>
+
+					<li>
+						<Link to="">
+							Log out
+						</Link>
+					</li>
+				</ul>
 			</div>
 		</header>
 		)
