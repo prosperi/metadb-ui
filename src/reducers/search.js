b91a4535ca38cd56322b7e7cffdd848e32913dfc
Adam Malantonio <malantoa@lafayette.edu>
2016-09-28 08:51:39 -0400

    add search reducer tests

--- src/reducers/search.js
+++ src/reducers/search.js
@@ -1,9 +1,36 @@
+/**
+ *  the search-state stores data related to the query itself -- results (which
+ *  include: facets, docs, page info) are handled within the results component
+ *  itself, as: a) that data doesn't need to be stored in the global state, and
+ *  b) it can easily be re-fetched using search info here.
+ *
+ *  note: all of the heavy lifting is being done within the action/search creator
+ *
+ *  {
+ *    // flagged when SEARCHING action is received
+ *    isSearching: bool
+ *
+ *    // contains the actual search query
+ *    query: string
+ *
+ *    // facets grouped by field
+ *    // { 'subject': [{value: 'art' ...}, {value: 'anthropology' ...} ]}
+ *    facets: object
+ *
+ *    // search options
+ *    // { 'per_page': 25 }
+ *    options: object
+ *
+ *		// the actual formatted querystring (used for pushState)
+ *    queryString: string
+ *  }
+ */
+
 import assign from 'object-assign'
 import {
 	RECEIVE_SEARCH_ERROR,
 	RECEIVE_SEARCH_RESULTS,
 	SEARCHING,
-	UPDATING_SEARCH,
 } from '../constants'
 
 export default function searchReducer (state, action) {
@@ -12,7 +39,6 @@ export default function searchReducer (state, action) {
 
 	switch (action.type) {
 		case SEARCHING:
-		case UPDATING_SEARCH:
 			return searching(state, action)
 
 		case RECEIVE_SEARCH_ERROR:
@@ -30,13 +56,12 @@ function searching (state, action) {
 		query: action.query,
 		facets: action.facets,
 		options: action.options,
-		results: [],
+		queryString: action.queryString,
 	}
 }
 
 function receiveResults (state, action) {
 	return assign({}, state, {
 		isSearching: false,
-		results: (action.error ? [] : action.results),
 	})
 }
