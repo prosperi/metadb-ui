27252b61eb9d6899180965f327a60e1dbf8e8067
Adam Malantonio <malantoa@lafayette.edu>
2016-11-14 11:19:37 -0500

    add metadata form components (string, text, date)
    - adds the following components:
    	- StringInput
    	- TextInput
    - moves DateInput from src/components/form-elements to src/components/metadata
    - removes tests from src/components/form-elements

---
+++ src/components/metadata/StringInput.jsx
@@ -0,0 +1,49 @@
+// used for single-line metadata. for larger blobs, use `TextInput`
+import React from 'react'
+import assign from 'object-assign'
+
+const StringInput = React.createClass({
+	handleBlur: function (ev) {
+		const val = ev.target.value
+		const check = this._initialValue ? this._initialValue : this.props.value
+
+		if (this.props.onBlur)
+			this.props.onBlur.apply(null, arguments)
+
+		if (this.props.disabled || this.props.readOnly || check === val)
+			return
+
+		this.props.onChange && this.props.onChange.call(null, val)
+	},
+
+	handleFocus: function (ev) {
+		this._initialValue = ev.target.value
+
+		if (this.props.onFocus)
+			this.props.onFocus.apply(null, arguments)
+	},
+
+	render: function () {
+		const props = assign(
+			// default
+			{type: 'text'},
+
+			// passed props
+			this.props,
+
+			// internal overrides
+			{
+				defaultValue: this.props.value || '',
+				onBlur: this.handleBlur,
+				onFocus: this.handleFocus,
+			}
+		)
+
+		// stick w/ defaultValue (which uses this.props.value)
+		delete props.value
+
+		return React.createElement('input', props)
+	}
+})
+
+export default StringInput
