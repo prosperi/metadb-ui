008b40000df2bbc5783858a3981093e7e64b0d72
adam malantonio <adam.malantonio@gmail.com>
2016-12-06 11:17:26 -0500

    update GenericWork form (#75)
    👍

--- src/components/metadata/DateInput.jsx
+++ src/components/metadata/DateInput.jsx
@@ -7,7 +7,9 @@ const DateInput = React.createClass({
 	propTypes: {
 		onChange: T.func,
 
-		style: T.object, 
+		style: T.object,
+
+		type: T.oneOf(['day', 'month', 'year']),
 
 		// expect ISO timestamp
 		value: T.string,
@@ -19,9 +21,12 @@ const DateInput = React.createClass({
 		if (!value)
 			return { date: null }
 
-		// `time` will either be `HH:MM:SS.000Z` 
+		// `time` will either be `HH:MM:SS.000Z`
 		// or undefined (if short date (YYYY-MM-DD) is passed)
-		const [date, time] = value.split('T')
+		let [ date ] = value.split('T')
+
+		if (this.props.type === 'month')
+			date = date.replace(/\-\d{2}$/, '')
 
 		return { date }
 	},
@@ -39,11 +44,19 @@ const DateInput = React.createClass({
 		return new Date(Date.UTC.apply(Date, values))
 	},
 
+	getInputType: function () {
+		switch (this.props.type) {
+			case 'month': return 'month'
+			default:
+				return 'date'
+		}
+	},
+
 	handleBlur: function () {
 		if (!this.props.onChange)
 			return
 
-		this.props.onChange && this.props.onChange.call(null, 
+		this.props.onChange && this.props.onChange.call(null,
 			this.getDateValue(), this.state.date
 		)
 	},
@@ -57,7 +70,8 @@ const DateInput = React.createClass({
 				display: 'inline-block',
 			}, this.props.style),
 
-			type: 'date',
+			type: this.getInputType(),
+			value: this.state.date,
 		}
 
 		return React.createElement('input', props)
--- src/components/schema/GenericWork.jsx
+++ src/components/schema/GenericWork.jsx
@@ -4,7 +4,49 @@ import React from 'react'
 import MetadataForm from '../metadata/MetadataForm.jsx'
 import FormField from '../metadata/FormField.jsx'
 import StringInput from '../metadata/StringInput.jsx'
+import TextInput from '../metadata/TextInput.jsx'
 import ControlledVocabularyInput from '../metadata/ControlledVocabularyInput.jsx'
+import DateInput from '../metadata/DateInput.jsx'
+import Button from '../Button.jsx'
+
+const labelFromName = name => {
+	const split = name.split('_').map(s => s.slice(0,1).toUpperCase() + s.slice(1))
+
+	if (split.length === 1)
+		return split[0]
+
+	return split.shift() + ' (' + split.join(', ') + ')'
+}
+
+const LargerField = props => {
+	if (!props.label)
+		props.label = labelFromName(props.name)
+
+	return <FormField {...props} renderer={TextInput} />
+}
+
+const SubjectOCM = props => {
+	const fetchTerms = () => {
+		return fetch(`${process.env.API_BASE_URL}/vocabularies/eaic-subject-ocm.json`)
+		.then(res => res.json())
+		.then(res => res.terms)
+	}
+
+	return (
+		<FormField {...props}
+			fetchTerms={fetchTerms}
+			highlightMatch
+			label="Subject (OCM)"
+			multiple
+			name="subject_ocm"
+			renderer={ControlledVocabularyInput}
+		/>
+	)
+}
+
+const TechnicalMetadata = props => {
+	return <FormField {...props} renderer={TextInput} disabled />
+}
 
 const GenericWork = function (props) {
 	const formProps = {
@@ -14,39 +56,30 @@ const GenericWork = function (props) {
 		...props,
 	}
 
-	const subjectOcmProps = {
-		fetchTerms: () => {
-			return fetch(process.env.API_BASE_URL + '/vocabularies/eaic-subject-ocm.json')
-			.then(res => res.json())
-			.then(res => res.terms)
-		},
-		highlightMatch: true,
-		multiple: true,
-		renderer: ControlledVocabularyInput,
-	}
-
 	return (
 		<MetadataForm {...formProps}>
 			<FormField name="title" label="Title" />
-			<FormField name="description_note" label="Description (Note)" />
+			{ LargerField({name: 'description_note'}) }
 			<FormField name="creator" label="Creator" multiple />
 			<FormField name="subject_lcsh" label="Subject (LCSH)" multiple />
-			<FormField name="subject_ocm" label="Subject (OCM)" {...subjectOcmProps}/>
+			{ SubjectOCM() }
 			<FormField name="publisher" label="Publisher (Original)" multiple />
-			<FormField name="date_original" label="Date (Original)" />
+			<FormField name="date_original" label="Date (Original)" renderer={DateInput} type="month"/>
 			<FormField name="format_medium" label="Format (Medium)" multiple />
-			<FormField name="format_extent" label="Format (Extent)" />
-			<FormField name="description" label="Description" />
-			<FormField name="description_condition" label="Description (Condition)" />
-			<FormField name="description_provenance" label="Description (Provenance)" />
-			<FormField name="description_series" label="Description (Series)" />
+			<TechnicalMetadata name="format_extent" label="Format Extent" />
+			{ LargerField({name: 'description'}) }
+			{ LargerField({name: 'description_condition'}) }
+			{ LargerField({name: 'description_provenance'}) }
+			{ LargerField({name: 'description_series'}) }
 			<FormField name="identifier_itemnumber" label="Identifier (Item Number)" />
 			<FormField name="publisher_original" label="Publisher (Original)" />
 			<FormField name="publisher_digital" label="Publisher (Digital)" />
-			<FormField name="format_digital" label="Format (Digital)" />
+			<TechnicalMetadata name="format_digital" label="Format (Digital)" />
 			<FormField name="source" label="Source" />
-			<FormField name="rights" label="Rights (Digital)" />
+			<FormField name="rights" label="Rights (Digital)" renderer={TextInput} />
 			<FormField name="relation_ispartof" label="Relation (IsPartOf)" />
+
+			<Button>Save edits</Button>
 		</MetadataForm>
 	)
 }
