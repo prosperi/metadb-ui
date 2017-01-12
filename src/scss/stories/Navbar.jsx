6e8eb043ac2932cf2635ec5b8c9162507ac4963c
adam malantonio <adam.malantonio@gmail.com>
2017-01-12 12:04:20 -0500

    remove @kadira/storybook devDependency (#107)
    👍

--- src/scss/stories/Navbar.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { storiesOf, action } from '@kadira/storybook'
-import Navbar from '../src/components/Navbar.jsx'
-
-storiesOf('<Navbar/>', module)
-	.add('renders well enough', () => (
-		<Navbar />
-	))
