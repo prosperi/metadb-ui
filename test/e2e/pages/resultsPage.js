dd53de41933291e09e0ceeeb5677f062b137a961
Zura <prosperrriii@gmail.com>
2017-02-23 10:20:50 -0500

    Integrated e2e testing

---
+++ test/e2e/pages/resultsPage.js
@@ -0,0 +1,16 @@
+export default {
+  elements: {
+    firstResultRow: {
+      selector: '.results-table tbody tr:first-child'
+    },
+    dateOriginal: {
+      selector: 'input[type="date"]'
+    },
+    facetPanel: {
+      selector: '.facet-panel:nth-of-type(6) header'
+    },
+    thumbnail: {
+      selector: '.thumbnail-preview'
+    }
+  }
+}
---
+++ test/e2e/tests/testSearchByDateRange.js
@@ -0,0 +1,32 @@
+export default {
+  'User Searches by Date': (client) => {
+    const searchPage = client.page.searchPage()
+    const resultsPage = client.page.resultsPage()
+
+    searchPage
+      .navigate()
+      .search('')
+
+    resultsPage.expect.element('@facetPanel').to.be.present
+    client.click('.facet-panel:nth-of-type(6) header')
+    resultsPage.expect.element('@dateOriginal').to.be.present
+
+		client.execute(() => {
+			document.getElementsByTagName('input')[2].setAttribute('max', '1945-08-15')
+			document.getElementsByTagName('input')[1].setAttribute('max', '1945-08-15')
+
+			document.getElementsByTagName('input')[2].setAttribute('min', '1785-08-26')
+			document.getElementsByTagName('input')[1].setAttribute('min', '1785-08-26')
+		})
+
+    resultsPage.expect.element('@dateOriginal').to.have.attribute('min').which.equals('1785-08-26')
+
+
+    resultsPage.expect.element('@dateOriginal').to.have.attribute('max').which.equals('1945-08-15')
+    client.click('.facet-panel:nth-of-type(6) button')
+
+    resultsPage.expect.element('@thumbnail').to.be.present
+		// test
+    client.end()
+  }
+}
