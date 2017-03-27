2d1845cd57b818b06376e126e4eedcf11fea9b36
Adam Malantonio <malantoa@lafayette.edu>
2017-03-27 10:23:17 -0400

    add promise/fetch polyfills to main test file

--- test.webpack.js
+++ test.webpack.js
@@ -1,9 +1,5 @@
-// we run into an issue with `fetch-mock` being called/required
-// before `isomorphic-fetch`, causing some ambigious errors and
-// failing every test that uses both. including `isomorphic-fetch`
-// here ensures that it will be defined first.
-// see: https://github.com/wheresrhys/fetch-mock/issues/60
-require('isomorphic-fetch')
+require('es6-promise').polyfill()
+import 'isomorphic-fetch'
 
 var srcContext = require.context('./src', true, /-test\.jsx?$/)
 srcContext.keys().forEach(srcContext)
